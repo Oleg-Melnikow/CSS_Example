b022ee68cec6df9a2ae9b1003dd3379f9a8a3d17
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2020-03-01 23:19:06 +0300

    add script

---
+++ PageNotFound/notfound.js
@@ -0,0 +1,6 @@
+let container = document.getElementById("found")
+window.onmousemove = function (e) {
+    let x = -e.clientX/5, y = -e.clientY/5
+    container.style.backgroundPositionX = x + "px"
+    container.style.backgroundPositionY = y + "px"
+}
